a6de8565bb80b5de1f86219bb32223d7af2ac9f4
Danielle <dlgeiser2992@gmail.com>
2020-05-06 11:28:25 -0400

    updated DB

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('wol-final', 'postgres', process.env.PASS, {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres'
 });
 
